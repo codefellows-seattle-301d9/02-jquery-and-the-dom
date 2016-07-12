23193828656ace1b66e624d97f9e1efcfccedcae
aziza alhouni <azizaalhouni@yahoo.com>
2016-07-12 16:04:25 -0700

    first two complete

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,19 +1,29 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the object passed in to complete this constructor function:
+  // Use the object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
   this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-category', this.title);
   $newArticle.attr('data-category', this.category);
-
-  /* TODO: Now use jQuery to fill in the rest of the current
+  $newArticle.attr('data-category', this.author);
+  $newArticle.attr('data-category', this.authorUrl);
+  $newArticle.attr('data-category', this.publishedOn);
+  $newArticle.attr('data-category', this.body);
+  /* Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
     1. author name,
+
     2. author url,
     3. article title
     4. article body, and
