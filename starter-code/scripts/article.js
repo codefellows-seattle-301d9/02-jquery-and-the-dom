93fbee1367724ad86d0b08135e3c51d2ada31f2b
TartarSauce <uma_bangalore@hotmail.com>
2016-07-12 16:05:08 -0700

    pair programming with logan, completed 3 of 6 TODOs

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,37 +1,39 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the object passed in to complete this constructor function:
+  // DONE: Use the object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`.
   this.author = opts.author;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery to fill in the rest of the current
-  template clone with properties from this particular Article instance.
-  We need to fill in:
-    1. author name,
-    2. author url,
-    3. article title
-    4. article body, and
-    5. publication date. */
 
+  /* DONE */
+  /* assign all elements that need to be assigned */
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))) + ' days ago');
+  $newArticle.find('address').html(this.author);
+  $newArticle.find('address a').attr('href', this.authorUrl);
+  $newArticle.attr('h1', this.title);
+  $newArticle.attr('data-category', this.category);
+  $newArticle.attr('article-body', this.body);
 
-  /* TODO: This clone article is no longer a template,
-  as it now has real data attached to it! We need to account
-  for that before this current article gets rendered to our
-  DOM. */
+  /* TODO : The clone article is no longer a template for it has real data
+  attached to it. We need to account for that before this current article
+  gets rendered to our DOM */
 
   return $newArticle;
 };
 
-ourLocalData.sort(function(curElem, nextElem) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+ourLocalData.sort(function(a, b){
+  return(new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
 ourLocalData.forEach(function(ele) {
